752718a93668729b749e17ecd6d78c34fe6d0d8a
shekharAggarwal <aggarwalshekhar99@gmail.com>
2022-09-28 12:47:55 +0530

    Multi View library for jetpack compose

---
+++ MultiView/src/main/java/com/shekhar/multiview/Previews.kt
@@ -0,0 +1,81 @@
+package com.shekhar.multiview
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Small Font",
+    group = "Small Font Group",
+    fontScale = 0.5f,
+    uiMode = UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Small Font",
+    group = "Small Font Group",
+    fontScale = 0.5f,
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true
+
+)
+@Preview(
+    name = "Normal Font",
+    group = "Normal Font Group",
+    uiMode = UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Normal Font",
+    group = "Normal Font Group",
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Preview(
+    name = "Large Font",
+    group = "Large Font Group",
+    fontScale = 1.5f,
+    uiMode = UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Large Font",
+    group = "Large Font Group",
+    fontScale = 1.5f,
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+annotation class CustomPreview
+
+@Preview(
+    name = "Pixel XL",
+    group = "Devices",
+    device = Devices.PIXEL_XL,
+    showSystemUi = true
+)
+@Preview(
+    name = "Pixel 2",
+    group = "Devices",
+    device = Devices.PIXEL_2,
+    showSystemUi = true
+)
+@Preview(
+    name = "Pixel 3",
+    group = "Devices",
+    device = Devices.PIXEL_3,
+    showSystemUi = true
+)
+@Preview(
+    name = "Pixel 4",
+    group = "Devices",
+    device = Devices.PIXEL_4,
+    showSystemUi = true
+)
+@Preview(
+    name = "Pixel C",
+    group = "Devices",
+    device = Devices.PIXEL_C,
+    showSystemUi = true
+)
+annotation class DevicePreview
---
+++ app/src/main/java/com/shekhar/devview/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.shekhar.devview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shekhar.devview.ui.theme.DevViewTheme
+import com.shekhar.multiview.CustomPreview
+import com.shekhar.multiview.DevicePreview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevViewTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+@CustomPreview
+@DevicePreview
+fun DefaultPreview() {
+    DevViewTheme {
+        Greeting("Android")
+    }
+}
